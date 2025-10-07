323eab74343edd48fdc406b62b93048507456012
risa walles <rmw395@nau.edu>
2025-10-07 15:39:39 -0700

    player code

---
+++ img420-project/Player.cs
@@ -0,0 +1,64 @@
+using Godot;
+// using System;
+
+public partial class Player : Area2D {
+	[Signal]
+	public delegate void HitEventHandler();
+
+	[Export]
+	public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
+
+	public Vector2 ScreenSize; // Size of the game window.
+	
+	public override void _Ready() {
+		ScreenSize = GetViewportRect().Size;
+		Hide();
+	} // _Ready
+
+	public override void _Process(double delta) {
+		var velocity = Vector2.Zero; // The player's movement vector.
+
+		// move inputs
+		if (Input.IsActionPressed("move_right")) {
+			velocity.X += 1;
+		}
+		if (Input.IsActionPressed("move_left")) {
+			velocity.X -= 1;
+		}
+		if (Input.IsActionPressed("move_down")) {
+			velocity.Y += 1;
+		}
+		if (Input.IsActionPressed("move_up")) {
+			velocity.Y -= 1;
+		} // end if (move inputs)
+
+		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+
+		if (velocity.Length() > 0) {
+			velocity = velocity.Normalized() * Speed;
+			animatedSprite2D.Play();
+		} else {
+			animatedSprite2D.Stop();
+		} // end if (velocity)
+		
+		Position += velocity * (float)delta;
+		Position = new Vector2(
+			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+		);
+	} // _Process
+	
+	public void Start(Vector2 position) {
+		Position = position;
+		Show();
+		GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
+	} // Start
+
+	// TODO: player loses health, doesn't just die
+	private void OnBodyEntered(Node2D body) {
+		Hide(); // Player disappears after being hit. TODO
+		EmitSignal(SignalName.Hit);
+		// Must be deferred as we can't change physics properties on a physics callback.
+		GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+	} // OnBodyEntered
+}
