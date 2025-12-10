815f5193dce8d48d1bf96ec4c33c1313c7f1109c
rmw395 <149126079+rmw395@users.noreply.github.com>
2025-12-10 12:53:09 -0700

    pig and human signals

--- img420-project-1/scripts/human.gd
+++ img420-project-1/scripts/human.gd
@@ -11,3 +11,7 @@ func _physics_process(delta):
 		var dir = (player.global_position - global_position).normalized()
 		velocity = dir * move_speed
 		move_and_slide()
+
+func _on_human_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.take_damage(10)
--- img420-project-1/scripts/pig.gd
+++ img420-project-1/scripts/pig.gd
@@ -14,3 +14,9 @@ func _process(delta):
 func _physics_process(delta):
 	velocity = wander_dir * move_speed
 	move_and_slide()
+
+
+func _on_pig_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.collect_blood(1)
+		queue_free()
